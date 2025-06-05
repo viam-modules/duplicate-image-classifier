ca46397b4d427c5cee743507d3e3ec4753d342d5
Adwait Ganguly <adwait.ganguly@viam.com>
2025-06-05 14:22:00 -0400

    fix formatting in setup.sh

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ if ! python3 -m venv $VENV_NAME >/dev/null 2>&1; then
         if ! command -v $SUDO >/dev/null; then
             SUDO=""
         fi
-		if ! apt info python3-venv >/dev/null 2>&1; then
+        if ! apt info python3-venv >/dev/null 2>&1; then
 			echo "Package info not found, trying apt update"
 			$SUDO apt -qq update >/dev/null
 		fi
