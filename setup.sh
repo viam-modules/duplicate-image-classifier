1335b9e88ba0c2e623ac970a18f249bbd1dc2706
Adwait Ganguly <adwait.ganguly@viam.com>
2025-06-05 14:23:04 -0400

    refix of setup.sh formatting

--- setup.sh
+++ setup.sh
@@ -15,9 +15,9 @@ if ! python3 -m venv $VENV_NAME >/dev/null 2>&1; then
             SUDO=""
         fi
         if ! apt info python3-venv >/dev/null 2>&1; then
-			echo "Package info not found, trying apt update"
-			$SUDO apt -qq update >/dev/null
-		fi
+            echo "Package info not found, trying apt update"
+            $SUDO apt -qq update >/dev/null
+        fi
         $SUDO apt install -qqy python3-venv >/dev/null 2>&1
         if ! python3 -m venv $VENV_NAME >/dev/null 2>&1; then
             echo $ENV_ERROR >&2
