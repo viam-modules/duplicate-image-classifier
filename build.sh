ff9d7edb3557b2d3c513693f7e1382b0c134980f
Adwait Ganguly <adwait.ganguly@viam.com>
2025-06-06 18:12:42 -0400

    fix comments extra hash in build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ cd `dirname $0`
 
 . venv/bin/activate
 
-## using `python -m PyInastaller` as opposed to `pyinstaller` as the former
-## runs in the venv and the latter does not
+# We use `python -m PyInastaller` as opposed to `pyinstaller` as the former
+# runs in the venv and the latter does not
 python -m PyInstaller --onefile --hidden-import="googleapiclient" src/main.py
 tar -czvf dist/archive.tar.gz ./dist/main meta.json
 # To run locally, we need meta.json in the same directory. So, add to dist/ a
