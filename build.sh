d96d9f511bb0cf0bb72825fbafad0c3414b32ee3
Adwait Ganguly <adwait.ganguly@viam.com>
2025-06-06 18:14:41 -0400

    remove googleapiclient as it was not necessary

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ cd `dirname $0`
 
 # We use `python -m PyInstaller` as opposed to `pyinstaller` as the former
 # runs in the venv and the latter does not
-python -m PyInstaller --onefile --hidden-import="googleapiclient" src/main.py
+python -m PyInstaller --onefile src/main.py
 tar -czvf dist/archive.tar.gz ./dist/main meta.json
 # To run locally, we need meta.json in the same directory. So, add to dist/ a
 # symlink that goes one directory up to meta.json
