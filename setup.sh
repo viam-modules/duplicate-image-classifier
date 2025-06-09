c770d8927fd6ab4c4fc933dbe4d9c894dd008473
Adwait Ganguly <adwait.ganguly@viam.com>
2025-06-09 09:32:40 -0400

    clean up the setup.sh file, adding comments and simplifying logic

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ VENV_NAME="venv"
 PYTHON="$VENV_NAME/bin/python"
 ENV_ERROR="This module requires Python, pip, and virtualenv to be installed."
 
-if ! python3 -m venv $VENV_NAME >/dev/null 2>&1; then
-    echo "Failed to create virtualenv."
+if ! python3 -m venv $VENV_NAME; then
+    echo "Failed to create virtualenv." >&2
     # If the virtualenv creation fails, we try to install python3-venv
     if command -v apt-get >/dev/null; then
         echo "Detected Debian/Ubuntu, attempting to install python3-venv automatically."
@@ -16,28 +16,38 @@ if ! python3 -m venv $VENV_NAME >/dev/null 2>&1; then
         if ! command -v $SUDO >/dev/null; then
             SUDO=""
         fi
-        if ! apt info python3-venv >/dev/null 2>&1; then
-            echo "Package info not found, trying apt update"
-            $SUDO apt -qq update >/dev/null
-        fi
-        $SUDO apt install -qqy python3-venv >/dev/null 2>&1
-        if ! python3 -m venv $VENV_NAME >/dev/null 2>&1; then
-            echo $ENV_ERROR >&2
+        # Run apt-get update by default, but only if it's available
+        echo "Updating package lists..."
+        if ! $SUDO apt-get update; then
+            echo "Failed to update package lists." >&2
             exit 1
         fi
+        # Install python3-venv, and gracefully exit if it fails
+        echo "Installing python3-venv..."
+        if ! $SUDO apt-get install -y python3-venv; then
+            echo "Failed to install python3-venv." >&2
+            exit 1
+        fi
+        # Create the virtual environment, and gracefully exit if it fails
+        if ! python3 -m venv $VENV_NAME; then
+            echo "$ENV_ERROR" >&2
+            exit 1
+        fi
+    # If the system does not have apt-get, we exit with an error
     else
-        echo $ENV_ERROR >&2
+        echo "$ENV_ERROR" >&2
         exit 1
     fi
 fi
 
-# remove -U if viam-sdk should not be upgraded whenever possible
-# -qq suppresses extraneous output from pip
-echo "Virtualenv found/created. Installing/upgrading Python packages..."
+echo "Virtualenv found/created. Installing Python packages..."
+# Check if packages are already installed
 if ! [ -f .installed ]; then
-    if ! $PYTHON -m pip install -r requirements.txt -Uqq; then
+    if ! $PYTHON -m pip install -r requirements.txt -q; then
+        echo "Failed to install Python packages. Please try again." >&2
         exit 1
     else
         touch .installed
+        echo "Python packages installed successfully."
     fi
 fi
