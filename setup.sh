09aae3793a639e479cb09dee9506c893b4a28017
Adwait Ganguly <adwait.ganguly@viam.com>
2025-06-10 10:01:00 -0400

    clean up indent in setup.sh

--- setup.sh
+++ setup.sh
@@ -8,29 +8,27 @@ ENV_ERROR="This module requires Python, pip, and virtualenv to be installed."
 # Attempt to create a virtual environment, and if it fails, try to install python3-venv
 if ! python3 -m venv $VENV_NAME; then
     echo "Failed to create virtualenv." >&2
-    # If the virtualenv creation fails, we try to install python3-venv
+    # Try to install python3-venv
     if command -v apt-get >/dev/null; then
-        echo "Detected Debian/Ubuntu, attempting to install python3-venv automatically."
-        SUDO="sudo"
-        # Run apt-get update by default, but only if it's available
-        echo "Updating package lists..."
-        if ! $SUDO apt-get update; then
-            echo "Failed to update package lists." >&2
-            exit 1
-        fi
-        # Install python3-venv, and gracefully exit if it fails
-        echo "Installing python3-venv..."
-        if ! $SUDO apt-get install -y python3-venv; then
-            echo "Failed to install python3-venv." >&2
-            exit 1
-        fi
-        # Create the virtual environment, and gracefully exit if it fails
-        if ! python3 -m venv $VENV_NAME; then
-            echo "$ENV_ERROR" >&2
-            exit 1
-        fi
-    # If the system does not have apt-get, we exit with an error
-    else
+        echo "$ENV_ERROR" >&2
+        exit 1
+    fi
+    echo "Detected Debian/Ubuntu, attempting to install python3-venv automatically."
+    SUDO="sudo"
+    # Run apt-get update by default, but only if it's available
+    echo "Updating package lists..."
+    if ! $SUDO apt-get update; then
+        echo "Failed to update package lists." >&2
+        exit 1
+    fi
+    # Install python3-venv, and gracefully exit if it fails
+    echo "Installing python3-venv..."
+    if ! $SUDO apt-get install -y python3-venv; then
+        echo "Failed to install python3-venv." >&2
+        exit 1
+    fi
+    # Create the virtual environment, and gracefully exit if it fails
+    if ! python3 -m venv $VENV_NAME; then
         echo "$ENV_ERROR" >&2
         exit 1
     fi
